92461967e7807991b6c6b418b67d7279bbdf8777
luis rendon <ljrendon@outlook.com>
2022-02-13 22:15:54 -0500

    first commit

---
+++ Strings.py
@@ -0,0 +1,9 @@
+my_string = "Welcome to Python!"
+another_string = 'The bright red fox jumped the fence.'
+a_long_string = '''This is a
+multi-line string. It covers more than
+one line'''
+
+print(my_string)
+print(another_string)
+print(a_long_string)
---
+++ _github.md
@@ -0,0 +1,7 @@
+# Github adding/commiting/pushing
+
+
+- git add .
+- git commit -m "write a msg of what you did for each commit"
+    ## example: (git commit -m "first commit")
+- git push
