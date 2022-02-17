6e8df6fc0ae57139d7303b857b2da3dbeb472f80
luis rendon <ljrendon@outlook.com>
2022-02-16 20:49:08 -0500

    lists

---
+++ Lists.py
@@ -0,0 +1,25 @@
+#creating a list
+my_list = []
+my_list = list()
+
+my_list = [1, 2, 3]
+my_list2 = [1, "Pyton", 3]
+
+#combining two lists
+#first way
+
+combo_list = [1, 2, 3]
+my_list = ["a",  "b", "c"]
+
+combo_list = combo_list + my_list
+print(combo_list)
+
+
+#sorting a list
+alpha_list = [1, 22, 12, 4, 34, 6, 41, 7, 15]
+alpha_list.sort()
+print(alpha_list)
+
+#slicing a list 
+my_list = [1, 2, 3, 4, 5]
+print(my_list[0:2])
